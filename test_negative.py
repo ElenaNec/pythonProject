02c6ac7148b4bebe0c3511e3d74b04f899c0cb84
ElenaNech <nechaeva_sam@mail.ru>
2023-11-15 14:04:05 +0300

    Add SEM_3 bad tests and fixture

--- test_negative.py
+++ test_negative.py
@@ -1,22 +1,17 @@
-import subprocess
-
-out = "/home/user/out"
-folder1 = "/home/user/folder1"
+import yaml
+from checkers import checkout_negative
 
 
-def checkout(cmd, text):
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding='utf-8')
-    if (text in result.stdout or text in result.stderr) and result.returncode != 0:
-        return True
-    else:
-        return False
+with open("config.yaml") as f:
+    data = yaml.safe_load(f)
 
 
-def test_step1():
-    # test1
-    assert checkout("cd {}; 7z e bad_arx.7z -o{} -y".format(out, folder1), "ERRORS"), "test1 FAIL"
+class TestPNegative:
 
+    def test_step1(self, make_bad_arx):
+        # test1
+        assert checkout_negative("cd {}; 7z e bad_arx.7z -o{} -y".format(data["folder_out"], data["folder_ext"]), "ERRORS"), "test1 FAIL"
 
-def test_step2():
-    # test2
-    assert checkout("cd {}; 7z t bad_arx.7z".format(out), "ERRORS"), "test2 FAIL"
+    def test_step2(self):
+        # test2
+        assert checkout_negative("cd {}; 7z t bad_arx.7z".format(data["folder_out"]), "ERRORS"), "test2 FAIL"
--- test_positive.py
+++ test_positive.py
@@ -1,67 +1,57 @@
 import subprocess
+import yaml
+from checkers import checkout
 
-tst = "/home/user/tst"
-out = "/home/user/out"
-folder1 = "/home/user/folder1"
-folder2 = "/home/user/folder2"
-
-
-def checkout(cmd, text):
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-    if text in result.stdout and result.returncode == 0:
-        return True
-    else:
-        return False
+with open('config.yaml') as f:
+    data = yaml.safe_load(f)
 
 
-def test_step1():
-    # test1
-    result1 = checkout("cd {}; 7z a {}/arx2".format(tst, out), "Everything is Ok")
-    result2 = checkout("cd {}; ls".format(out), "arx2.7z")
-    assert result1 and result2, "test1 FAIL"
+class TestPositive:
+    def test_step1(self):
+        # test1
+        result1 = checkout("cd {}; 7z a {}/arx2".format(data["folder_in"], data["folder_out"]), "Everything is Ok")
+        result2 = checkout("ls {}".format(data["folder_out"]), "arx2.7z")
+        assert result1 and result2, "test1 FAIL"
+
+    def test_step2(self, make_files):
+        # test2
+        result1 = checkout("cd {}; 7z e arx2.7z -o{} -y".format(data["folder_out"], ["folder_ext"]), "Everything is Ok")
+        result2 = checkout("cd {}; ls".format(data["folder_ext"]), make_files[0])
+        print(checkout("ls", make_files[0] ))
+        assert result1 and result2, "test2 FAIL"
+
+    def test_step3(self):
+        # test3
+        assert checkout("cd {}; 7z t arx2.7z".format(data["folder_out"]), "Everything is Ok"), "test3 FAIL"
+
+    def test_step4(self):
+        # test4
+        assert checkout("cd {}; 7z u {}/arx2.7z".format(data['folder_in'], data['folder_out']), "Everything is Ok"), "test4 FAIL"
+
+    def test_step6(self):
+        # test6
+        result1 = checkout("cd {}; 7z l arx2.7z".format(data['folder_out']), "qwe")
+        result2 = checkout("cd {}; ls".format(data['folder_in']), "qwe")
+        result3 = checkout("cd {}; ls".format(data['folder_in']), "rty")
+        assert result1 and result2 and result3, "test6 FAIL"
+
+    def test_step7(self, data_make):
+        # test7
+        result1 = checkout("cd {}; 7z x arx2.7z -o{} -y".format(data['folder_out'], data['folder_ext2']),
+                           "Everything is Ok")
+        result2 = checkout("cd {}; ls".format(data['folder_ext2']), data_make[0])
+        assert result1 and result2, "test7 FAIL"
 
 
-def test_step2():
-    # test2
-    result1 = checkout("cd {}; 7z e arx2.7z -o{} -y".format(out, folder1), "Everything is Ok")
-    result2 = checkout("cd {}; ls".format(folder1), "qwe")
-    result3 = checkout("cd {}; ls".format(folder1), "rty")
-    assert result1 and result2 and result3, "test2 FAIL"
+    def test_step8(self):
+        # test8
+        result1 = subprocess.run("cd {}; 7z h arx2.7z".format(data['folder_out']), shell=True, stdout=subprocess.PIPE,
+                                 encoding='utf-8')
+        result2 = subprocess.run("cd {}; crc32 arx2.7z".format(data['folder_out']), shell=True, stdout=subprocess.PIPE,
+                                 encoding='utf-8')
+        assert result1.stdout and result2.stdout, "test8 FAIL"
 
 
-def test_step3():
-    # test3
-    assert checkout("cd {}; 7z t arx2.7z".format(out), "Everything is Ok"), "test3 FAIL"
-
-
-def test_step4():
-    # test4
-    assert checkout("cd {}; 7z u {}/arx2.7z".format(tst, out), "Everything is Ok"), "test4 FAIL"
-
-
-def test_step6():
-    # test6
-    result1 = checkout("cd {}; 7z l arx2.7z".format(out), "qwe")
-    result2 = checkout("cd {}; ls".format(tst), "qwe")
-    result3 = checkout("cd {}; ls".format(tst), "rty")
-    assert result1 and result2 and result3, "test6 FAIL"
-
-
-def test_step7():
-    # test7
-    result1 = checkout("cd {}; 7z x arx2.7z -o{} -y".format(out, folder2), "Everything is Ok")
-    result2 = checkout("cd {}; ls".format(folder2), "qwe")
-    result3 = checkout("cd {}; ls".format(folder2), "rty")
-    assert result1 and result2 and result3, "test7 FAIL"
-
-
-def test_step8():
-    # test8
-    result1 = subprocess.run("cd {}; 7z h arx2.7z".format(out), shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-    result2 = subprocess.run("cd {}; crc32 arx2.7z".format(out), shell=True, stdout=subprocess.PIPE, encoding='utf-8')
-    assert result1.stdout and result2.stdout, "test8 FAIL"
-
-
-def test_step5():
-    # test5
-    assert checkout("cd {}; 7z d arx2.7z".format(out), "Everything is Ok"), "test5 FAIL"
+    def test_step5(self):
+        # test5
+        assert checkout("cd {}; 7z d arx2.7z".format(data['folder_out']), "Everything is Ok"), "test5 FAIL"
